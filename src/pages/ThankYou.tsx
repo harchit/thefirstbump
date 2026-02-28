e23a5d5c0e1202fdc814dc2c3fabf2f07d710fb4
[dyad] <harchit23@gmail.com>
2026-02-28 11:20:50 -0500

    [dyad] Update Purchase event value - wrote 1 file(s)

--- src/pages/ThankYou.tsx
+++ src/pages/ThankYou.tsx
@@ -39,7 +39,7 @@ const ThankYou = () => {
             content_name: 'The First Bump - A New Mom\'s Pregnancy Guide',
             content_type: 'product',
             currency: 'USD',
-            value: 29.99
+            value: 24.99
           }, { eventID: sessionId });
         }
         
@@ -55,7 +55,7 @@ const ThankYou = () => {
         content_name: 'The First Bump - A New Mom\'s Pregnancy Guide',
         content_type: 'product',
         currency: 'USD',
-        value: 29.99
+        value: 24.99
       });
     }
 
