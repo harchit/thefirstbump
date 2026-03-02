2724d364dded1612881cfef90651fae5b801005b
[dyad] <harchit23@gmail.com>
2026-03-02 13:05:47 -0500

    [dyad] Removing duplicate close button - wrote 1 file(s)

--- src/components/snippet-modal.tsx
+++ src/components/snippet-modal.tsx
@@ -1,8 +1,7 @@
 "use client";
 
-import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogClose } from "@/components/ui/dialog";
+import { Dialog, DialogContent, DialogHeader, DialogTitle } from "@/components/ui/dialog";
 import { Button } from "@/components/ui/button";
-import { X } from "lucide-react";
 
 interface SnippetModalProps {
   isOpen: boolean;
@@ -19,12 +18,6 @@ const SnippetModal = ({ isOpen, onClose }: SnippetModalProps) => {
       <DialogContent className="max-w-3xl w-full h-[90vh] p-0 flex flex-col bg-white rounded-lg shadow-2xl">
         <DialogHeader className="p-4 border-b flex-shrink-0">
           <DialogTitle className="text-lg font-medium text-gray-800">Chapter Snippet</DialogTitle>
-          <DialogClose asChild>
-            <Button variant="ghost" size="icon" className="absolute top-3 right-3 rounded-full">
-              <X className="h-5 w-5" />
-              <span className="sr-only">Close</span>
-            </Button>
-          </DialogClose>
         </DialogHeader>
         <div className="flex-grow p-2 bg-gray-100">
           <iframe
